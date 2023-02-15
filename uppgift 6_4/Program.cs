c202b5707ab57b83f4487c60aa4f5f8f27c33940
Meysam03 <alre03001@utb.vaxjo.se>
2023-02-15 12:04:31 +0100

    Add project files.

---
+++ uppgift 6_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uppgift6_4
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+
+            double värde = Potens();
+            Console.WriteLine("Värdet blir " + värde);
+
+        }
+
+        static double Potens()
+        {
+            Console.WriteLine("Skriv in basen till ditt tal");
+            double bas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bra! Skriv nu in exponent till basen");
+            double exponent = int.Parse(Console.ReadLine());
+
+            return Math.Pow(bas, exponent);
+        }
+    }
+}
